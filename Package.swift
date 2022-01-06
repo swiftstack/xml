8db7966431385f23ca14e3aba5eed4978be99274
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 09:03:02 +0300

    [Package] Add minimum target requirements

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "XML",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
         .library(
             name: "XML",
